ae03b8d205520e8bafe36390f0f6b0959c7789e0
rkcbalanza <102932930+rkcbalanza@users.noreply.github.com>
2022-10-04 15:59:27 +0800

    working film category

--- index.js
+++ index.js
@@ -17,10 +17,70 @@ const connection = mysql.createConnection({
 //
 let router = express.Router();
 
-//
-router.get('/', function (req, res, next) {
+//Film APIs
+//Get ALL films
+router.get('/film/', function (req, res, next) {
+    connection.query(
+        'SELECT * FROM film',
+        function (err, results, fields) {
+            if(err) throw err;
+            res.send(results);
+        }
+    );
+});
+
+//Get films by title
+router.get('/film/title=:title', function (req, res, next) {
+    connection.query(
+        "SELECT * FROM film WHERE title LIKE ?",
+        ["%"+req.params.title+"%"],
+        function (err, results, fields) {
+            res.send(results);
+        }
+    );
+});
+
+
+//Get films by category
+router.get('/film/category=:category', function (req, res, next) {
+    connection.query(
+        "SELECT category.name AS category, film.title AS title, film.description AS description FROM category JOIN film_category ON category.category_id = film_category.category_id JOIN film ON film.film_id = film_category.film_id WHERE category.name = ?",
+        [req.params.category],
+        function (err, results, fields) {
+            res.send(results);
+        }
+    );
+});
+
+//Actor APIs
+//Get ALL actors
+router.get('/actor/', function (req, res, next) {
     connection.query(
         'SELECT * FROM actor',
+        function (err, results, fields) {
+            if(err) throw err;
+            res.send(results);
+        }
+    );
+});
+
+//Get actor by first_name
+router.get('/actor/first_name=:firstName', function (req, res, next) {
+    connection.query(
+        'SELECT * FROM actor WHERE first_name = ?',
+        [req.params.firstName],
+        function (err, results, fields) {
+            res.send(results);
+        }
+    );
+});
+
+
+//Get actor by first_name
+router.get('/actor/last_name=:lastName', function (req, res, next) {
+    connection.query(
+        'SELECT * FROM actor WHERE last_name = ?',
+        [req.params.lastName],
         function (err, results, fields) {
             res.send(results);
         }
